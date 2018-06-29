e09c8b1e8fe655213fa340d54434d21b68b3b5fb
Francisco Alvarez <s2globalmail@gmail.com>
2018-06-29 16:09:46 -0400

    Images and profile added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  resources :characters
-  resources :abilities
-  resources :items
-  devise_for :users, controllers: {:omniauth_callbacks => "callbacks"}
+  get 'user/show'
+  get 'users/show'
+	get 'user/show'
+	get 'home/index'
+	resources :characters
+	resources :abilities
+	resources :items
+	devise_for :users, controllers: {:omniauth_callbacks => "callbacks"}
 
-  root "home#index"
+	get 'users/:id' => 'user#show'
+	root "home#index"
 end
