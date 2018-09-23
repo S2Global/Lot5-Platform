38b7ab56b958ca162a90a13c3ad7b601ac6a55a9
Francisco Alvarez <s2globalmail@gmail.com>
2018-09-22 23:10:58 -0300

    gem bundler updated, check for exists

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Archetype.create(name: "test archetype",
-            description: "test description",
-            hp_level: 6,
-            skill_1: "sk1",
-            skill_2: "sk2",
-            skill_3: "sk3",
-            skill_4: "sk4",
-            skill_5: "sk5",
-            skill_6: "sk6",
-            skill_7: "sk7",
-            skill_8: "sk8",
-            skill_9: "sk9",
-            skill_10: "sk10")
+      description: "test description",
+      hp_level: 6,
+      skill_1: "sk1",
+      skill_2: "sk2",
+      skill_3: "sk3",
+      skill_4: "sk4",
+      skill_5: "sk5",
+      skill_6: "sk6",
+      skill_7: "sk7",
+      skill_8: "sk8",
+      skill_9: "sk9",
+      skill_10: "sk10")
 
 Race.create(name: "test race",
-			lore: "test lore",
-			advantage: "test advantage")
+	lore: "test lore",
+	advantage: "test advantage")
